2558f988de23b3338889bae69fdefc7705f95ab1
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-02-01 23:53:01 -0700

    cart added products/items

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import LayOut from '../../Component/LayOut/LayOut'
+import { DataContext } from '../../Component/DataProvider/DataProvider'
+import ProductCart from '../../Component/Products/ProductCard'
 
 function Cart() {
+    const [{basket, user }, dispatch] = useContext(DataContext);
     return (
         <LayOut> 
-            <div>Cart</div>
+            <section>
+                <div>
+                    <h2>Hello</h2>
+                    <h3>Your Shopping Basket</h3>
+                    <hr />
+                    {
+                        basket?.length==0?(<p>Opps ! No item in your cart</p>):(basket?.map((item,i)=>{
+                            return <ProductCart
+                            key={i}
+                            product={item}
+                            renderDesc={true}
+                            flex={true}
+                            />
+                        }))
+                    }
+                </div>
+                <div></div>
+            </section>
         </LayOut>
     )
 }

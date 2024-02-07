00ad6a33e1d3beaf7d38b4e9c286f7b8daa5e2d2
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-02-07 14:45:55 -0700

    Setting up backend payment API using Firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,45 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config()
+const stripe = require("stripe")(process.env.SSTRIPE_KEY);
+
+
+const app = express()
+app.use(cors({origin:true}))
+
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "success !",
+    });
+});
+
+app.post("/payment/create", async(req, res)=> {
+    const total = req.query.total
+    if(total > 0){
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total,
+            currency: "usd"
+        });        
+        // console.log(paymentIntent)
+        res.status(201).json({
+            cliantSecret: paymentIntent.client_secret,
+        });
+    }else {
+        res.status(403).json({
+            messaging: "total must be greater than 0"
+        });
+    }
+})
+
+
+
+exports.api = onRequest(app)
+
 
 
